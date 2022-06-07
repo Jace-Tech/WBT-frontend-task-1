734585a3bc86539525fb5d314ce078fbd116dd71
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-06-07 12:16:12 +0100

    Solved the quiz

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function Slides() {
+function Slides({ slides }) {
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    const handleRestart = () => setCurrentIndex(0)
+
+    const handlePrev = () => {
+        if(currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1)
+        }
+    }
+
+    const handleNext = () => {
+        if(currentIndex < slides.length) {
+            setCurrentIndex(currentIndex + 1)
+        }
+    }
+
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button data-testid="button-restart" onClick={handleRestart} disabled={currentIndex === 0} className="small outlined">Restart</button>
+                <button data-testid="button-prev" onClick={handlePrev} disabled={currentIndex < 1} className="small outlined">Prev</button>
+                <button data-testid="button-next" onClick={handleNext} disabled={currentIndex >= slides.length - 1} className="small">Next</button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide title here</h1>
-                <p data-testid="text">Slide texts here</p>
+                <h1 data-testid="title"> { slides[currentIndex].title }</h1>
+                <p data-testid="text">{ slides[currentIndex].text }</p>
             </div>
         </div>
     );
